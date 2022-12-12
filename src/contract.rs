e6337913f236fb4c1799a98764da276f57b70ec2
Jan Woźniak <jnwznk@gmail.com>
2022-12-12 22:04:03 +0100

    Contract tests utils

--- src/contract.rs
+++ src/contract.rs
@@ -25,3 +25,124 @@ impl CounterContract<'_> {
         Ok(Response::new())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use anyhow::bail;
+    use cosmwasm_std::{from_slice, Addr, DepsMut, Empty, Env, MessageInfo, Response};
+    use cw_multi_test::{App, Contract, Executor};
+
+    use crate::counter;
+
+    use super::{
+        ContractError, ContractExecMsg, ContractQueryMsg, CounterContract, InstantiateMsg,
+    };
+
+    impl Contract<Empty> for CounterContract<'_> {
+        fn execute(
+            &self,
+            deps: DepsMut<Empty>,
+            env: Env,
+            info: MessageInfo,
+            msg: Vec<u8>,
+        ) -> anyhow::Result<Response<Empty>> {
+            from_slice::<ContractExecMsg>(&msg)?
+                .dispatch(self, (deps, env, info))
+                .map_err(Into::into)
+        }
+
+        fn instantiate(
+            &self,
+            deps: DepsMut<Empty>,
+            env: Env,
+            info: MessageInfo,
+            msg: Vec<u8>,
+        ) -> anyhow::Result<Response<Empty>> {
+            from_slice::<InstantiateMsg>(&msg)?
+                .dispatch(self, (deps, env, info))
+                .map_err(Into::into)
+        }
+
+        fn query(
+            &self,
+            deps: cosmwasm_std::Deps<Empty>,
+            env: Env,
+            msg: Vec<u8>,
+        ) -> anyhow::Result<cosmwasm_std::Binary> {
+            from_slice::<ContractQueryMsg>(&msg)?
+                .dispatch(self, (deps, env))
+                .map_err(Into::into)
+        }
+
+        fn sudo(
+            &self,
+            _deps: DepsMut<Empty>,
+            _env: Env,
+            _msg: Vec<u8>,
+        ) -> anyhow::Result<Response<Empty>> {
+            bail!("sudo not implemented for contract")
+        }
+
+        fn reply(
+            &self,
+            _deps: DepsMut<Empty>,
+            _env: Env,
+            _msg: cosmwasm_std::Reply,
+        ) -> anyhow::Result<Response<Empty>> {
+            bail!("sudo not implemented for contract")
+        }
+
+        fn migrate(
+            &self,
+            _deps: DepsMut<Empty>,
+            _env: Env,
+            _msg: Vec<u8>,
+        ) -> anyhow::Result<Response<Empty>> {
+            bail!("sudo not implemented for contract")
+        }
+    }
+
+    #[derive(Clone, Copy, Debug, PartialEq, Eq)]
+    pub struct CounterCodeId(u64);
+
+    impl CounterCodeId {
+        pub fn store_code(app: &mut App) -> Self {
+            let code_id = app.store_code(Box::new(CounterContract::new()));
+            Self(code_id)
+        }
+
+        pub fn code_id(&self) -> u64 {
+            self.0
+        }
+
+        #[track_caller]
+        pub fn instantiate(
+            self,
+            app: &mut App,
+            sender: &Addr,
+            label: &str,
+            admin: Option<String>,
+        ) -> Result<CounterProxy, ContractError> {
+            let msg = InstantiateMsg {};
+
+            app.instantiate_contract(self.0, sender.clone(), &msg, &[], label, admin)
+                .map_err(|err| err.downcast().unwrap())
+                .map(CounterProxy)
+        }
+    }
+
+    #[derive(Debug)]
+    pub struct CounterProxy(Addr);
+
+    impl CounterProxy {
+        // cw20-base
+        #[track_caller]
+        pub fn increase_counter(&self, app: &mut App, sender: &Addr) -> Result<(), ContractError> {
+            let msg = counter::ExecMsg::IncreaseCount {};
+
+            app.execute_contract(sender.clone(), self.0.clone(), &msg, &[])
+                .map_err(|err| err.downcast().unwrap())
+                .map(|_| ())
+        }
+    }
+}
